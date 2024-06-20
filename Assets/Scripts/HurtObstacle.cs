72b4c40314cee13309094add5d4685a39203a36d
JokarTheFirst <142092410+JokarTheFirst@users.noreply.github.com>
2024-06-20 10:24:03 +0300

    Added Enemy Prefab

--- Assets/Scripts/HurtObstacle.cs
+++ Assets/Scripts/HurtObstacle.cs
@@ -7,7 +7,7 @@ public class HurtObstacle : MonoBehaviour
     [SerializeField] float damage = 10f;
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Hit!");
+        
         if (other.CompareTag("Player"))
         {
             PlayerStats playerStats = other.GetComponent<PlayerStats>();
