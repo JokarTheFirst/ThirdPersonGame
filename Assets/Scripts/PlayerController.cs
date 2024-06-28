143326ac39f1410ef4c1c7feb1a9463dfe3ff8ee
JokarTheFirst <142092410+JokarTheFirst@users.noreply.github.com>
2024-06-28 13:29:23 +0300

    Tweaked Victory Screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
+
 using static UnityEngine.ParticleSystem;
 
 [RequireComponent(typeof(CharacterController))]
